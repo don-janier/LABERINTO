06984aae90a44dc073718c9c3373e8cff84405da
Dayfred <dayfredadm1234@gmail.com>
2026-05-14 22:30:11 -0500

    POR FIN AVANCES (programados los botones y se agrego pygame para usar sus funciones para el personaje)

--- main/main.py
+++ main/main.py
@@ -3,7 +3,24 @@ import tkinter as tk
 ventana = tk.Tk()
 ventana.title("La Berintonela")
 ventana.geometry("1000x600")
-ventana.configure(bg="Purple4")
-etiqueta = tk.Label(ventana, text="LA BERINTONELA", font=("Fixedsys", 30, "bold"), fg="white", bg="black", height=-2, width=20   )
-etiqueta.pack(pady=20)
+ventana.configure(bg="#100221")
+ventana.resizable(False, False)
+
+def empezar_juego():
+    for elemento in ventana.winfo_children():
+        elemento.destroy()
+    ventana.configure(bg="#100221")
+
+def cerrar_programa():
+    ventana.destroy()
+
+etiqueta1 = tk.Label(ventana, text="LA BERINTONELA", font=("Fixedsys", 50, "bold"), fg="#6CEBEB", bg="#100221", height=-2, width=20   )
+etiqueta1.pack(pady=20)
+
+boton_play = tk.Button(ventana, text="PLAY", font=("Fixedsys", 20, "bold"), command=empezar_juego, fg="#000000", width=10, height=2)
+boton_play.pack(pady=100)
+
+boton_exit = tk.Button(ventana, text="EXIT", font=("Fixedsys", 20, "bold"), command=cerrar_programa, fg="#000000", width=10, height=2)
+boton_exit.pack(pady=10)
+
 ventana.mainloop()
